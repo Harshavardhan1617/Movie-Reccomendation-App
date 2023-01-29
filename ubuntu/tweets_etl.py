c7bb55bdb40de18ee1b181abb9e6cff6b4507b9c
Harshavardhan1617 <harshavardhan1617@gmail.com>
2023-01-29 21:18:05 +0530

    automated database update using cron

--- ubuntu/tweets_etl.py
+++ ubuntu/tweets_etl.py
@@ -174,3 +174,14 @@ if __name__ == "__main__":
 
         conn.close()
         print("Close database successfully")
+
+        from datetime import datetime
+
+        # datetime object containing current date and time
+        now = datetime.now()
+
+        print("now =", now)
+
+        # dd/mm/YY H:M:S
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print("date and time =", dt_string)
--- ubuntu/tweets_to_db.py
+++ ubuntu/tweets_to_db.py
@@ -158,3 +158,14 @@ except:
 
 conn.close()
 print("Close database successfully")
+
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
